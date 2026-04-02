d19c8c18fe091e96a27c771d887a60fadf220595
Bogi168 <danielbogner5@gmail.com>
2026-04-02 22:24:52 +0200

    update

--- Schafkopf.py
+++ Schafkopf.py
@@ -1,8 +1,9 @@
 from Renderer import Renderer
-from Cards import Cards
+from Cards import Cards, Color
+from Player import Player
 from Game import Sauspiel, Wenz, Solo
 from handle_players import create_players, choose_starter, sort_players
-from handle_game_decision import play_game_decision, choose_game_decision
+from handle_game_decision import play_game_decision, check_available_game_decisions, check_available_sau_color_decisions, convert_sau_color_value, convert_sau_color_index
 from handle_cards import prepare_cards
 
 
@@ -18,22 +19,53 @@ class Schafkopf:
         self.cards = Cards()
         self.renderer = renderer
 
+    def choose_game_decision(self, player: Player, prev_game):
+        player_name = player.player_name
+        player_cards = player.player_cards
+        available_decisions = check_available_game_decisions(playable_games = self.playable_games, prev_game=prev_game, player_cards=player_cards)
+        decision = self.renderer.player_choose_game(player_name)
+        while decision not in available_decisions:
+            decision = self.renderer.reask_player_game(player_name=player_name)
+        match decision:
+            case "1":
+                sau_colors = [Color.EICHEL, Color.GRUEN, Color.SCHELLEN]
+                available_colors = check_available_sau_color_decisions(player_cards=player_cards,
+                                                                       playable_colors=sau_colors.copy())
+                sau_color_decision = self.renderer.player_choose_sau_color()
+                sau_color_value = convert_sau_color_value(decision=sau_color_decision)
+                sau_color_index = convert_sau_color_index(decision=sau_color_decision)
+                while (sau_color_value not in [color.value for color in sau_colors]
+                       or sau_colors[sau_color_index] not in available_colors):
+                    sau_color_decision = self.renderer.player_rechoose_sau_color()
+                    sau_color_value = convert_sau_color_value(decision=sau_color_decision)
+                    sau_color_index = convert_sau_color_index(decision=sau_color_decision)
+                sau_color = sau_colors[sau_color_index]
+                self.game_mode = Sauspiel(cards=self.cards, renderer=self.renderer, players=self.players, sau_color=sau_color)
+            case "2":
+                self.game_mode = Wenz(cards=self.cards, renderer=self.renderer, players=self.players)
+            case "3":
+                trump_color = self.renderer.player_choose_solo_color()
+                while trump_color not in ("1", "2", "3", "4"):
+                    trump_color = self.renderer.player_rechoose_solo_color()
+                match trump_color:
+                    case "1":
+                        trump_color = Color.EICHEL
+                    case "2":
+                        trump_color = Color.GRUEN
+                    case "3":
+                        trump_color = Color.HERZ
+                    case "4":
+                        trump_color = Color.SCHELLEN
+                self.game_mode = Solo(trump_color=trump_color, cards=self.cards, renderer=self.renderer, players=self.players)
+
     def players_choose_game(self):
         if len(self.game_choosers) == 0:
             self.game_mode = None
         else:
             for player in self.game_choosers:
-                if player == self.game_choosers[0]:
-                    self.game_mode = choose_game_decision(renderer=self.renderer, player=player,
-                                                     playable_games=self.playable_games,
-                                                     cards=self.cards, players=self.players, prev_game=None)
-                    self.game_chooser = player
-                else:
-                    # Fehlt: Wenn Vorgänger spielt Wenz oder Solo -> Abbruch möglich bzw. zwingend
-                    self.game_mode = choose_game_decision(renderer=self.renderer, player=player,
-                                                     playable_games=self.playable_games,
-                                                     cards=self.cards, players=self.players, prev_game=self.game_mode)
-                    self.game_chooser = player
+                # Fehlt: Wenn Vorgänger spielt Wenz oder Solo -> Abbruch möglich bzw. zwingend
+                self.choose_game_decision(player=player, prev_game=self.game_mode)
+                self.game_chooser = player
 
     def main(self):
         self.players = create_players(renderer=self.renderer)
--- handle_game_decision.py
+++ handle_game_decision.py
@@ -1,7 +1,7 @@
 from Player import Player
 from Renderer import Renderer
-from Cards import Cards, Color, Type
-from Game import Game, Sauspiel, Wenz, Solo
+from Cards import Color, Type
+from Game import Game
 
 def play_game_decision(player: Player, renderer: Renderer, game_choosers: list):
     decision = renderer.ask_player_game(player_name=player.player_name)
@@ -98,44 +98,6 @@ def convert_sau_color_index(decision: str) -> int:
             sau_color_decision = 2
     return sau_color_decision
 
-def choose_game_decision(playable_games: list, renderer: Renderer, player: Player, cards: Cards, players: list, prev_game) -> Game:
-    player_name = player.player_name
-    player_cards = player.player_cards
-    available_decisions = check_available_game_decisions(playable_games=playable_games, prev_game=prev_game, player_cards=player_cards)
-    decision = renderer.player_choose_game(player_name)
-    while decision not in available_decisions:
-        decision = renderer.reask_player_game(player_name=player_name)
-    match decision:
-        case "1":
-            sau_colors = [Color.EICHEL, Color.GRUEN, Color.SCHELLEN]
-            available_colors = check_available_sau_color_decisions(player_cards=player_cards, playable_colors=sau_colors.copy())
-            sau_color_decision = renderer.player_choose_sau_color()
-            sau_color_value = convert_sau_color_value(decision=sau_color_decision)
-            sau_color_index = convert_sau_color_index(decision=sau_color_decision)
-            while (sau_color_value not in [color.value for color in sau_colors]
-                   or sau_colors[sau_color_index] not in available_colors):
-                sau_color_decision = renderer.player_rechoose_sau_color()
-                sau_color_value = convert_sau_color_value(decision=sau_color_decision)
-                sau_color_index = convert_sau_color_index(decision=sau_color_decision)
-            sau_color = sau_colors[sau_color_index]
-            game_mode = Sauspiel(cards=cards, renderer=renderer, players=players, sau_color=sau_color)
-        case "2":
-            game_mode = Wenz(cards=cards, renderer=renderer, players=players)
-        case "3":
-            trump_color = renderer.player_choose_solo_color()
-            while trump_color not in ("1", "2", "3", "4"):
-                trump_color = renderer.player_rechoose_solo_color()
-            match trump_color:
-                case "1":
-                    trump_color = Color.EICHEL
-                case "2":
-                    trump_color = Color.GRUEN
-                case "3":
-                    trump_color = Color.HERZ
-                case "4":
-                    trump_color = Color.SCHELLEN
-            game_mode = Solo(trump_color=trump_color, cards=cards, renderer=renderer, players=players)
-    return game_mode
 
 if __name__== "__main__":
     pass
