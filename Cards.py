669b496eaa86a97d5251ed3e21aabb12b095a0b1
Bogi168 <danielbogner5@gmail.com>
2026-04-03 15:56:10 +0200

    update

--- Cards.py
+++ Cards.py
@@ -69,7 +69,7 @@ class Card:
 
 class Cards:
     def __init__(self):
-        self.full_deck = [Card(card_color = card_color, card_type = card_type, card_rank = card_type.value)
+        self.full_deck = [Card(card_color=card_color, card_type=card_type, card_rank=card_type.value)
                           for card_type in Type for card_color in Color]
         self.deck = copy.deepcopy(self.full_deck)
 
