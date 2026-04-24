124e6e73288f289541208afc7242db57bfbcfd60
Bogi168 <danielbogner5@gmail.com>
2026-04-24 16:14:42 +0200

    update

--- system/text.py
+++ system/text.py
@@ -18,7 +18,7 @@ def show_player_cards(player_name: str, player_cards: list[Card]):
 
 
 def show_played_card(player_name: str, decision: Card):
-    return f"\n{player_name} played the card: {decision}"
+    return f"\n{player_name} played the card: {decision.card_name}"
 
 
 def show_played_cards(played_cards: list[Card]):
