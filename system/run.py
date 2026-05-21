caba153428be1e92b967df66d1a9eaa1b83654e9
Bogi168 <danielbogner5@gmail.com>
2026-05-21 14:06:31 +0200

    __name__ implementation

--- system/run.py
+++ system/run.py
@@ -4,4 +4,6 @@ from game_classes.Schafkopf import Schafkopf
 game = Schafkopf(
     renderer=ConsoleRenderer(), base_price=10, call_price=20, alone_price=30
 )
-game.main()
+
+if __name__ == "__main__":
+    game.main()
