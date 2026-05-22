2ce01b434d29fa08ff32518806dee26c29be28c9
Bogi168 <danielbogner5@gmail.com>
2026-05-22 20:40:01 +0200

    update

--- system/run.py
+++ system/run.py
@@ -1,9 +1,8 @@
 from system.Renderer import ConsoleRenderer
 from game_classes.Schafkopf import Schafkopf
 
-game = Schafkopf(
-    renderer=ConsoleRenderer(), base_price=10, call_price=20, alone_price=30
-)
-
 if __name__ == "__main__":
+    game = Schafkopf(
+        renderer=ConsoleRenderer(), base_price=10, call_price=20, alone_price=30
+    )
     game.main()
